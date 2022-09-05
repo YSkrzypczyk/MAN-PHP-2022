5ac51694e98abb9d15bab7e89f7323575d6f3b3b
Skrzypczyk <ysk@wizards-technologies.com>
2022-09-05 14:59:36 +0200

    Tp routing

--- www/index.php
+++ www/index.php
@@ -1,1 +1,16 @@
 <?php
+
+    //Récupérer l'url, exemple /login
+
+    //Ouverture de notre fichier de routing
+
+    //Récupérer le controller et l'action pour l'url
+    // exemple /login controller: Security et l'action login
+
+    //Créer une instance de la class controller
+    //Appeler la méthode action
+
+    //Exemple :
+    // $controller = new Security();
+    // $controller->login();
+
